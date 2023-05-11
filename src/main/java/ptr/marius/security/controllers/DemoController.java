7257159ffa648622d05ef2c602440776d2ca5416
Marius Patru <mpatru@deloittece.com>
2023-05-11 18:09:37 +0300

    first steps

---
+++ src/main/java/ptr/marius/security/controllers/DemoController.java
@@ -0,0 +1,15 @@
+package ptr.marius.security.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello world!";
+    }
+
+    
+}
