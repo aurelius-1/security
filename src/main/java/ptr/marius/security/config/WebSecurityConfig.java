1b64c7e6c3142602e58c499a671421a779ce572e
aurelius-1 <leaurica92@gmail.com>
2023-05-14 19:37:30 +0300

    understood authentication provider overriding

--- src/main/java/ptr/marius/security/config/WebSecurityConfig.java
+++ src/main/java/ptr/marius/security/config/WebSecurityConfig.java
@@ -4,14 +4,12 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import ptr.marius.security.config.security.filters.CustomAuthenticationFilter;
+
 
 @Configuration
 @AllArgsConstructor
@@ -20,30 +18,16 @@ public class WebSecurityConfig {
     private final CustomAuthenticationFilter customAuthenticationFilter;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
-
-        return httpSecurity
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
                 .addFilterAt(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
-                .authorizeHttpRequests().anyRequest().authenticated()
-                .and()
-                .build();
+                .authorizeRequests().anyRequest().authenticated()  // don't worry about this
+                .and().build();
     }
 
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        var uds = new InMemoryUserDetailsManager();
-//        var u1 = User.withUsername("bill")
-//                .password("12345")
-//                .authorities("read")
-//                .build();
-//
-//        uds.createUser(u1);
-//
-//        return uds;
-//    }
-
-//    @Bean
-//    public PasswordEncoder passwordEncoder() {
-//        return NoOpPasswordEncoder.getInstance();
-//    }
+    //lesson learnt --- custom authentication overwrites password encoded authentication!!!
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
