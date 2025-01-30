804ff6f5d588d8c777e52e9a8bdb0b227ab62aa7
aurelius-1 <leaurica92@gmail.com>
2025-01-30 10:50:58 +0200

    in memory user details service

--- src/main/java/ptr/marius/security/configuration/Config.java
+++ src/main/java/ptr/marius/security/configuration/Config.java
@@ -4,23 +4,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import ptr.marius.security.service.InMemoryUserDetailsService;
+import ptr.marius.security.user.User;
+
+import java.util.List;
 
 @Configuration
 public class Config {
 
     @Bean
     UserDetailsService userDetailsService() {
-        var user = User.withUsername("john")
-                .password("12345")
-                .authorities("read")
-                .build();
-        return new InMemoryUserDetailsManager(user);
+       UserDetails user = new User("john", "12345", "read");
+       List<UserDetails> users = List.of(user);
+
+        return new InMemoryUserDetailsService(users);
     }
 
     @Bean
